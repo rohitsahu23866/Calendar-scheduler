a43299368d67e45af3e414175ab34c25206ec476
rohitsahu23866 <76580992+rohitsahu23866@users.noreply.github.com>
2024-08-19 13:15:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calendar from './components/Calendar';
 import EventForm from './components/EventForm';
 import EventDetails from './components/EventDetails';
+import Footer from './components/Footer'; // Import Footer component
 import { EventProvider } from './contexts/EventContext';
 
 function App() {
   return (
     <EventProvider>
       <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Calendar />} />
-            <Route path="/event/:id" element={<EventDetails />} />
-            <Route path="/event/add" element={<EventForm />} />
-            <Route path="/event/edit/:id" element={<EventForm />} />
-          </Routes>
+        <div className="App min-h-screen flex flex-col">
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Calendar />} />
+              <Route path="/event/:id" element={<EventDetails />} />
+              <Route path="/event/add" element={<EventForm />} />
+              <Route path="/event/edit/:id" element={<EventForm />} />
+            </Routes>
+          </div>
+          <Footer /> 
         </div>
       </Router>
     </EventProvider>
